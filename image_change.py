59afc839ba9b2a22c542deca8b112f31c7f803d2
Anbyoungyeul <qudduf9313@gmail.com>
2022-05-23 17:48:01 +0900

    main_add

--- image_change.py
+++ image_change.py
@@ -5,16 +5,16 @@ import np
 import codecs
 import glob
 
-sourcepath = r"D:\disasmbled\train__benign\\"
-destpath = r"D:\image\\"
-count =1
+
+
 
 def convert_asm_to_images(sourcepath, destpath):
     files = os.listdir(sourcepath) # 모든 파일과 디렉토리 리턴 
     print('Sourcepath :', sourcepath)
     print('Destination path :', destpath)
+    count =1
     for file in files:
-        if file.glob.golb("\*.asm"):
+        if file.endswith(".asm"):
             f = codecs.open(sourcepath+file, 'rb')
             ln = os.path.getsize(sourcepath+file)
             width = int(ln**0.5)
@@ -26,7 +26,10 @@ def convert_asm_to_images(sourcepath, destpath):
             g = np.uint8(g)
             cv2.imwrite(destpath+file+'.png', g)
             print(str(count) +'번째 :' + file)
-            ++count
+            count += 1
     print('Files converted sucessfully')
 
-convert_asm_to_images(sourcepath, destpath)
+def convert_start():
+    sourcepath = r"D:\disasmbled\train__benign\\"
+    destpath = r"D:\image\\"
+    convert_asm_to_images(sourcepath, destpath)
---
+++ main.py
@@ -0,0 +1,4 @@
+import image_change
+
+if __name__=="__main__":
+    image_change.convert_start()
